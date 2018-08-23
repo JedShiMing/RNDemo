92dafe98a2c44094c0f5cecefca458f12eadeea9
jed@yunio.com <Sm123456>
2018-08-23 15:48:09 +0800

    1

--- src/view/test1/test1.screen.js
+++ src/view/test1/test1.screen.js
@@ -1,74 +1,59 @@
 import React from 'react'
 import {
-    Platform,
     StyleSheet,
-    Text,
-    View, Animated
+    View, NativeModules
 } from 'react-native';
-import {Label, Overlay, Button} from 'teaset'
+import { Button } from 'teaset'
 
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
+// const ShareDialog = NativeModules.FBShareDialog
+const FBSDK = require('react-native-fbsdk');
+// const {
+//   ShareDialog,
+// } = FBSDK;
+// import {ShareDialog} from 'react-native-fbsdk'
+const shareLinkContent = {
+    contentType: 'link',
+    contentUrl: "https://facebook.com",
+    contentDescription: 'Wow, check out this great site!',
+};
 export default class Test1Screen extends React.Component {
-    state = {
-        _posLeft: new Animated.Value(0)
+    constructor(props) {
+        super(props);
+        this.state = {
+            shareLinkContent: shareLinkContent
+        }
     }
-    _Animated
-
-    componentWillMount() {
-        _Animated = Animated.decay(this.state._posLeft, {
-            toValue: 200,
-        })
-    }
-
     render() {
-        const {_posLeft} = this.state
         return (
             <View style={styles.container}>
-                <Button title={'点击出来弹框'} onPress={() => this.showPopView()}/>
-                <Button title={'点击移动长方形'} onPress={() => {
-                    console.log('_postLeft = ', _posLeft._value)
-                    if (_posLeft._value >= 400) {
-                        Animated.timing(_posLeft, {
-                            toValue: 0,
-                        }).start()
-                    } else {
-                        Animated.timing(_posLeft, {
-                            toValue: 400,
-                        }).start()
-                    }
-                }}/>
-                <Animated.View
-                    style={{backgroundColor: 'green', height: 30,marginTop: 30, position: 'relative', right: _posLeft}}>
-                    <Text>hhhhhhhhhhhhhhhhh</Text>
-                </Animated.View>
+                <Button title={'分享到facebook'} onPress={() => this.shareLinkWithShareDialog()} />
             </View>
         );
     }
 
-    showPopView() {
-        let overlayView = (
-            <Overlay.PopView
-                style={{alignItems: 'center', justifyContent: 'center'}}
-            >
-                <View style={{
-                    backgroundColor: '#fff',
-                    minWidth: 260,
-                    minHeight: 180,
-                    borderRadius: 15,
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}>
-                    <Label type='title' size='xl' text='Pop Overlay'/>
-                </View>
-            </Overlay.PopView>
-        )
-        Overlay.show(overlayView)
+    // Share the link using the share dialog.
+    shareLinkWithShareDialog() {
+        console.log('分享到fb')
+        let tmp = this;
+        ShareDialog.canShow(tmp.state.shareLinkContent).then(
+            function (canShow) {
+                if (canShow) {
+                    return ShareDialog.show(tmp.state.shareLinkContent);
+                }
+            }
+        ).then(
+            function (result) {
+                if (result.isCancelled) {
+                    alert('Share cancelled');
+                } else {
+                    alert('Share success with postId: '
+                        + result.postId);
+                }
+            },
+            function (error) {
+                alert('Share fail with error: ' + error);
+            }
+        );
     }
 }
 
